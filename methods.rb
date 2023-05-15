60b76b89b56cf5ec01c6900b6cdddb6b60fb22cf
kevin-wairi <kevinwairi@gmail.com>
2023-05-15 23:09:35 +0300

    assignment completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,37 @@
 # Your code here!
+
+# def stylish_painter
+#   best_hairstyle = "Bob Ross"
+#   puts "Bob Ross"
+#   return "Jean-Michel Basquiat"
+#   best_hairstyle
+# end
+
+# stylish_painter
+
+def greet_programmer
+    puts "Hello, programmer!" 
+end
+ def greet name
+    puts "Hello, #{name}!"
+ end
+ greet("Naureen")
+  greet("Jimmy")
+
+ def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+ end
+greet_with_default("John")
+
+def add num1,num2
+    return num1 + num2
+end
+add(2,2)
+
+def halve num
+    if num != num.to_i
+        return nil
+    else
+        return num/2
+    end
+end
